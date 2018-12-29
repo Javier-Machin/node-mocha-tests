ce1b049dc8e27c64c7dfdb27ae50347882d455a9
Javier Machin <javier_machin_acosta@hotmail.com>
2018-12-29 13:36:47 +0000

    Add expect assertion library

--- utils/utils.js
+++ utils/utils.js
@@ -2,7 +2,16 @@ module.exports.add = (a, b) => a + b;
 
 module.exports.square = (x) => x * x;
 
+module.exports.setName = (user, fullName) => {
+  const name = fullName.split(" ");
+  user.firstName = name[0];
+  user.lastName = name[1];
+
+  return user;
+};
+
 // module.exports = {
 //   add,
-//   square
+//   square,
+//   setName
 // };
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,48 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   const res = utils.add(33, 11);
 
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}`);
-  }
+  expect(res).toBe(44).toBeA('number');
+  // if (res !== 44) {
+  //   throw new Error(`Expected 44, but got ${res}`);
+  // }
 });
 
 it('should square a number', () => {
   const res = utils.square(3);
 
-  if (res !== 9) {
-    throw new Error(`Expected 9, but got ${res}`);
-  }
+  expect(res).toBe(9).toBeA('number');
+  // if (res !== 9) {
+  //   throw new Error(`Expected 9, but got ${res}`);
+  // }
 });
+
+it('should set user firstName and lastName.', () => {
+  const user = utils.setName({ age: 100, location:'Spain' },'Javier Machin');
+
+  expect(user)
+    .toBeA('object')
+    .toInclude({ firstName: 'Javier', lastName: 'Machin'});
+});
+
+
+// it('should expect some values', () =>{
+//   expect(12).toNotBe(11);
+
+//   expect({name: 'Javi'}).toEqual({name: 'Javi'});
+  
+//   expect([2,3,4]).toInclude(2);
+  
+//   expect([2,3,4]).toExclude(6);
+
+//   expect({
+//     name: 'Javi',
+//     age: 100,
+//     location: 'Spain'
+//   }).toInclude({
+//     age: 100
+//   })
+// });
