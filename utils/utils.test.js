5bfd685fcdf771dc60ee0ce3cfe5bf7188144e12
Javier Machin <javier_machin_acosta@hotmail.com>
2018-12-30 13:27:10 +0000

    Add describe blocks

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,39 +2,47 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-  const res = utils.add(33, 11);
+describe('Utils', () => {
 
-  expect(res).toBe(44).toBeA('number');
-  // if (res !== 44) {
-  //   throw new Error(`Expected 44, but got ${res}`);
-  // }
-});
+  describe('#add', () => {
+    it('should add two numbers', () => {
+      const res = utils.add(33, 11);
+    
+      expect(res).toBe(44).toBeA('number');
 
-// Add keyword done for async testing
-it('should async add two numbers', (done) => {
-  utils.asyncAdd(4, 3, (sum) => {
-    expect(sum).toBe(7).toBeA('number');
-    done();
+      // if (res !== 44) {
+      //   throw new Error(`Expected 44, but got ${res}`);
+      // }
+    });
   });
-})
 
-it('should square a number', () => {
-  const res = utils.square(3);
-
-  expect(res).toBe(9).toBeA('number');
-  // if (res !== 9) {
-  //   throw new Error(`Expected 9, but got ${res}`);
-  // }
-});
-
-// Add keyword done for async testing
-it('should async square a number', (done) => {
-  utils.asyncSquare(3, (res) => {
+  // Add keyword done for async testing
+  it('should async add two numbers', (done) => {
+    utils.asyncAdd(4, 3, (sum) => {
+      expect(sum).toBe(7).toBeA('number');
+      done();
+    });
+  })
+  
+  it('should square a number', () => {
+    const res = utils.square(3);
+  
     expect(res).toBe(9).toBeA('number');
-    done();
+    
+    // if (res !== 9) {
+    //   throw new Error(`Expected 9, but got ${res}`);
+    // }
+  });
+  
+  // Add keyword done for async testing
+  it('should async square a number', (done) => {
+    utils.asyncSquare(3, (res) => {
+      expect(res).toBe(9).toBeA('number');
+      done();
+    });
   });
 });
+
 
 it('should set user firstName and lastName.', () => {
   const user = utils.setName({ age: 100, location:'Spain' },'Javier Machin');
